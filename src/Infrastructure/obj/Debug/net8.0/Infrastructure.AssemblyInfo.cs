f74bc171c8e92df5b08239e7037c347dab55522b
Izengard <jocdanlm@gmail.com>
2025-10-27 01:06:24 -0500

    AssemblyInfo

--- src/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7197a7d20f610856da5fc5fa222fe4c41a48fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf84a7413931a6c065ada3897bd9eed65e353c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
