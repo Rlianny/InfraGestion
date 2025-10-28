c0b01313adf668817c545203f3a24b9fa811a99b
Rlianny <revelianny10@gmail.com>
2025-10-27 23:21:04 -0400

    feat(entities): Business Logic implemented

--- src/Domain/Entities/SectionManager.cs
+++ src/Domain/Entities/SectionManager.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Interfaces;
 
 namespace Domain.Entities
 {
     public class SectionManager : User
     {
-        public SectionManager(string fullName, string passwordHash,int departmentID) : base(fullName, passwordHash,departmentID)
-        {
-        }
-        private SectionManager() : base()
-        {
-        }
         public int SectionID { get; private set; }
+        List<int> ManagedSectionsIds = new List<int>();
+        public SectionManager(string fullName, string passwordHash, int departmentID, int sectionID) : base(fullName, passwordHash, departmentID)
+        {
+            SectionID = sectionID;
+        }
+        private SectionManager() : base() { }
 
+        public bool CanManageSection(int sectionID)
+        {
+            return ManagedSectionsIds.Contains(sectionID);
+        }
     }
 }
