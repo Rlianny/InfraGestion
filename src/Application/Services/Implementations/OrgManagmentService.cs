1a5caff07989460136b5161f225dc1cc375f5802
Kevin Torres <kevintorresperera@gmail.com>
2025-12-01 02:31:12 -0500

    feat:Get departments and section OrgManagement

--- src/Application/Services/Implementations/OrgManagmentService.cs
+++ src/Application/Services/Implementations/OrgManagmentService.cs
@@ -14,12 +14,12 @@ namespace Application.Services.Implementations
 {
     public class OrgManagmentService : IOrgManagementService
     {
-        private  readonly ISectionRepository sectionRepository;
+        private readonly ISectionRepository sectionRepository;
         private readonly IDepartmentRepository departmentRepository;
         private readonly IUnitOfWork unitOfWork;
         private readonly IUserRepository userRepository;
 
-        public OrgManagmentService(ISectionRepository sectionRepository,IDepartmentRepository departmentRepository,IUnitOfWork unitOfWork,
+        public OrgManagmentService(ISectionRepository sectionRepository, IDepartmentRepository departmentRepository, IUnitOfWork unitOfWork,
             IUserRepository userRepository)
         {
             this.sectionRepository = sectionRepository;
@@ -31,9 +31,9 @@ namespace Application.Services.Implementations
         public async Task AssignSectionResponsible(AssignSectionResponsibleDto assignSectionResponsible)
         {
             var section = await sectionRepository.GetByIdAsync(assignSectionResponsible.SectionId) ??
-                throw new EntityNotFoundException("Section",assignSectionResponsible.SectionId);
-            var user = await userRepository.GetByIdAsync(assignSectionResponsible.UserId)??
-                throw new EntityNotFoundException("User",assignSectionResponsible.UserId);
+                throw new EntityNotFoundException("Section", assignSectionResponsible.SectionId);
+            var user = await userRepository.GetByIdAsync(assignSectionResponsible.UserId) ??
+                throw new EntityNotFoundException("User", assignSectionResponsible.UserId);
             section.AssignManager(user);
             user.ChangeRole((int)RoleEnum.SectionManager);
             await sectionRepository.UpdateAsync(section);
@@ -43,7 +43,7 @@ namespace Application.Services.Implementations
 
         public async Task CreateDepartment(DepartmentDto departmentDto)
         {
-            await departmentRepository.AddAsync(new Department(departmentDto.Name,departmentDto.SectionId));
+            await departmentRepository.AddAsync(new Department(departmentDto.Name, departmentDto.SectionId));
             await unitOfWork.SaveChangesAsync();
         }
 
@@ -55,21 +55,21 @@ namespace Application.Services.Implementations
 
         public async Task DisableDepartment(DepartmentDto departmentDto)
         {
-            var department = await departmentRepository.GetByIdAsync(departmentDto.DepartmentId)??
+            var department = await departmentRepository.GetByIdAsync(departmentDto.DepartmentId) ??
                 throw new EntityNotFoundException("Deparment", departmentDto.DepartmentId);
             await departmentRepository.DeleteAsync(department);
             await unitOfWork.SaveChangesAsync();
-            
+
         }
 
         public async Task DisableSection(SectionDto sectionDto)
         {
 
-            var section = await sectionRepository.GetByIdAsync(sectionDto.SectionId)??
+            var section = await sectionRepository.GetByIdAsync(sectionDto.SectionId) ??
                 throw new EntityNotFoundException("Section", sectionDto.SectionId);
             await sectionRepository.DeleteAsync(section);
             await unitOfWork.SaveChangesAsync();
-            
+
         }
 
         public async Task ModifyDepartment(DepartmentDto departmentDto)
@@ -87,6 +87,37 @@ namespace Application.Services.Implementations
                  throw new EntityNotFoundException("Section", sectionDto.SectionId);
             await sectionRepository.UpdateAsync(section);
             await unitOfWork.SaveChangesAsync();
+        }
+        public async Task<IEnumerable<SectionDto>> GetSectionsAsync()
+        {
+            var sections = await sectionRepository.GetAllAsync();
+            var sectionDtos = new List<SectionDto>();
+            foreach (var section in sections)
+            {
+                var dto = new SectionDto
+                {
+                    SectionId = section.SectionId,
+                    Name = section.Name
+                };
+                sectionDtos.Add(dto);
+            }
+            return sectionDtos;
+        }
+        public async Task<IEnumerable<DepartmentDto>> GetDepartmentsAsync()
+        {
+            var departments = await departmentRepository.GetAllAsync();
+            var departmentDtos = new List<DepartmentDto>();
+            foreach (var department in departments)
+            {
+                var dto = new DepartmentDto
+                {
+                    DepartmentId = department.DepartmentId,
+                    Name = department.Name,
+                    SectionId = department.SectionId
+                };
+                departmentDtos.Add(dto);
+            }
+            return departmentDtos;
         }
     }
 }
