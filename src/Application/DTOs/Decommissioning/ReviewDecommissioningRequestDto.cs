20ac8d6683934c3a7cea125d7f91ad120fbbce62
Kevin Torres <kevintorresperera@gmail.com>
2025-12-05 16:12:37 -0500

    feat : Implement DecommissioningService

--- src/Application/DTOs/Decommissioning/ReviewDecommissioningRequestDto.cs
+++ src/Application/DTOs/Decommissioning/ReviewDecommissioningRequestDto.cs
@@ -1,14 +1,16 @@
+using Domain.Enums;
+
 namespace Application.DTOs.Decommissioning
 {
     public class ReviewDecommissioningRequestDto
     {
         public int DecommissioningRequestId { get; set; }
         public bool IsApproved { get; set; }
-        public string RejectionReason { get; set; } // if is rejected
+        public string? RejectionReason { get; set; } // if is rejected
         
         // if is approved
+        public DecommissioningReason? DecommissioningReason { get; set; } // EOL, IrreparableTechnicalFailure, etc.
         public string? FinalDestination { get; set; } // "Almacén", "Desecho", "Reciclaje", etc.
         public int? ReceiverDepartmentId { get; set; }
-        public string? ReceiverName { get; set; }
     }
 }
