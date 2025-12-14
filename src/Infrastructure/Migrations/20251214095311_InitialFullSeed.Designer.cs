059428531c6f75c4be26d39e0aa62122be003b0e
Rlianny <revelianny10@gmail.com>
2025-12-14 04:53:49 -0500

    chore: Update

--- src/Infrastructure/Migrations/20251214095311_InitialFullSeed.Designer.cs
+++ src/Infrastructure/Migrations/20251214095311_InitialFullSeed.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251213081013_InitialFullSeed")]
+    [Migration("20251214095311_InitialFullSeed")]
     partial class InitialFullSeed
     {
         /// <inheritdoc />
